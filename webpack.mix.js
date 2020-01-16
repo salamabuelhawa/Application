ccb1a11cba4076f829320ddfcdcbab455e7e91bf
salam <salammohd.11@gmail.com>
2020-01-16 13:44:45 +0200

    I am Confused

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,20 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css');
+
+
+mix.styles(['resources/assets/css/libs/blog-post.css',
+                'resources/assets/css/libs/bootstrap.css',
+                'resources/assets/css/libs/font-awesome.css',
+                'resources/assets/css/libs/metisMenu.css',
+                'resources/assets/css/libs/sb-admin-2.css',
+                'resources/assets/css/libs/style.css',
+],'public/css/libs.css');
+
+
+mix.scripts(['resources/assets/css/libs/bootstrap.js',
+                'resources/assets/css/libs/jquery.js',
+                'resources/assets/css/libs/metisMenu.js',
+                'resources/assets/css/libs/sb-admin-2.js',
+                'resources/assets/css/libs/scripts.js',
+],'public/js/libs.js');
