a7808052b2bbec22d5d5738467d09781875b2a02
salam <salammohd.11@gmail.com>
2020-01-16 12:21:32 +0200

    everything is good until now

--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    //Mine
+    public function role(){
+        return $this->belongsTo('App\Role');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//Mine
+Route::resource('admin/users','AdminUsersController');
